30cb98d3e7bc8e144e2a0bcd62479046b8eae797
Julien <julien@tvty.tv>
2017-01-03 14:29:45 +0100

    Fix composer cache directory

--- composer/composer.sh
+++ composer/composer.sh
@@ -6,5 +6,8 @@ COMPOSER_ARGS=$@
 # Retrieve local user UID and group GID
 set -- `ls -nd .` && LOCAL_UID=$3 && LOCAL_GID=$4
 
+# Update rights on cache dir
+chown $LOCAL_UID:$LOCAL_GID /composer-cache-dir
+
 # Run composer command with the correct rights
-sudo -g \#$LOCAL_GID -u \#$LOCAL_UID composer $COMPOSER_ARGS
+sudo -g \#$LOCAL_GID -u \#$LOCAL_UID -E composer $COMPOSER_ARGS
