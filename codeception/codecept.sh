3dc6a98c12ae3634f33d142cbafc559bb3193ebb
Jeckel <jeckel@jeckel.fr>
2017-01-12 22:24:48 +0100

    Fix codeception launcher script

--- codeception/codecept.sh
+++ codeception/codecept.sh
@@ -6,7 +6,5 @@ CMD_ARGS=$@
 # Retrieve local user UID and group GID
 set -- `ls -nd .` && LOCAL_UID=$3 && LOCAL_GID=$4
 
-which codecept
-
 # Run command with the correct rights
 sudo -g \#$LOCAL_GID -u \#$LOCAL_UID -E /repo/codecept $CMD_ARGS
